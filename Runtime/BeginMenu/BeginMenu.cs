d0973f471d4287c0278c99ad84cbe09e3cf2f3fd
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-14 23:02:10 +0000

    Add empty BeginMenu

---
+++ Runtime/BeginMenu/BeginMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using RPGFramework.Core;
+
+namespace RPGFramework.Menu.BeginMenu
+{
+    public class BeginMenu : IBeginMenu
+    {
+        Task IMenu.OnEnterAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        Task IMenu.OnExitAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
