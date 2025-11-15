e593b998a3920177cc80761e58d7cd8fc91bc2eb
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-15 21:32:23 +0000

    MenuUIProvider now returns a VisualTreeAsset per type

--- Runtime/MenuUIProvider.cs
+++ Runtime/MenuUIProvider.cs
@@ -1,11 +1,23 @@
+using System;
 using RPGFramework.Core;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace RPGFramework.Menu
 {
     [CreateAssetMenu(fileName = "Menu UI Provider", menuName = "RPG Framework/Menu/Menu UI Provider")]
     public class MenuUIProvider : ScriptableObject, IMenuUIProvider
     {
-    
+        [SerializeField]
+        private VisualTreeAsset m_BeginMenu;
+
+        VisualTreeAsset IMenuUIProvider.GetMenuUI<T>()
+        {
+            Type type = typeof(T);
+
+            if (type == typeof(IBeginMenu)) return m_BeginMenu;
+
+            throw new NotImplementedException($"The type {type} is not implemented");
+        }
     }
 }
