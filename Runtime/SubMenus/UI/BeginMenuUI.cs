ba58351f5e5333238a2357d4256b44bed86c5746
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-15 22:05:37 +0000

    Add BeginMenuUI class

---
+++ Runtime/SubMenus/UI/BeginMenuUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using RPGFramework.Core;
+using UnityEngine.UIElements;
+
+namespace RPGFramework.Menu.SubMenus.UI
+{
+    public class BeginMenuUI : IBeginMenuUI
+    {
+        private readonly IMenuUIProvider m_UIProvider;
+
+        public BeginMenuUI(IMenuUIProvider uiProvider)
+        {
+            m_UIProvider = uiProvider;
+        }
+
+        Task IMenuUI.OnEnterAsync(VisualElement rootContainer)
+        {
+            VisualTreeAsset uiAsset = m_UIProvider.GetMenuUI<IBeginMenu>();
+            uiAsset.CloneTree(rootContainer);
+
+            return Task.CompletedTask;
+        }
+
+        Task IMenuUI.OnSuspendAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        Task IMenuUI.OnResumeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        Task IMenuUI.OnExitAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
