2bb2f7cc2f16de958497a28862195c206ed37b74
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-15 21:33:05 +0000

    BeginMenu now instantiates its UI asset

--- Runtime/SubMenus/BeginMenu.cs
+++ Runtime/SubMenus/BeginMenu.cs
@@ -1,12 +1,23 @@
 ﻿using System.Threading.Tasks;
 using RPGFramework.Core;
+using UnityEngine.UIElements;
 
 namespace RPGFramework.Menu.SubMenus
 {
     public class BeginMenu : IBeginMenu
     {
-        Task IMenu.OnEnterAsync()
+        private readonly IMenuUIProvider m_UIProvider;
+
+        public BeginMenu(IMenuUIProvider uiProvider)
         {
+            m_UIProvider = uiProvider;
+        }
+
+        Task IMenu.OnEnterAsync(VisualElement rootContainer)
+        {
+            VisualTreeAsset uiAsset = m_UIProvider.GetMenuUI<IBeginMenu>();
+            uiAsset.CloneTree(rootContainer);
+
             return Task.CompletedTask;
         }
 
