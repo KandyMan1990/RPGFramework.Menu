b8aa5497a9bcbb65594598249391ff8431bfcdb7
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-15 11:53:01 +0000

    Add empty Menu UI Provider

---
+++ Runtime/MenuUIProvider.cs
@@ -0,0 +1,11 @@
+using RPGFramework.Core;
+using UnityEngine;
+
+namespace RPGFramework.Menu
+{
+    [CreateAssetMenu(fileName = "Menu UI Provider", menuName = "RPG Framework/Menu/Menu UI Provider")]
+    public class MenuUIProvider : ScriptableObject, IMenuUIProvider
+    {
+    
+    }
+}
