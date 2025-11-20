9071747cbc1dae313eafbd35e79d27df3dd9603e
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-20 22:33:54 +0000

    Update BeginMenu to play sfx on UI events

--- Runtime/SubMenus/BeginMenu.cs
+++ Runtime/SubMenus/BeginMenu.cs
@@ -6,31 +6,46 @@ namespace RPGFramework.Menu.SubMenus
 {
     public class BeginMenu : IBeginMenu
     {
+        private readonly IMenuModule  m_MenuModule;
         private readonly IBeginMenuUI m_BeginMenuUI;
 
-        public BeginMenu(IBeginMenuUI beginMenuUI)
+        public BeginMenu(IMenuModule menuModule, IBeginMenuUI beginMenuUI)
         {
+            m_MenuModule  = menuModule;
             m_BeginMenuUI = beginMenuUI;
         }
 
         Task IMenu.OnEnterAsync(VisualElement rootContainer)
         {
+            m_BeginMenuUI.OnPlayAudio += PlaySfx;
+
             return m_BeginMenuUI.OnEnterAsync(rootContainer);
         }
 
         Task IMenu.OnSuspendAsync()
         {
+            m_BeginMenuUI.OnPlayAudio -= PlaySfx;
+
             return Task.CompletedTask;
         }
 
         Task IMenu.OnResumeAsync()
         {
+            m_BeginMenuUI.OnPlayAudio += PlaySfx;
+
             return Task.CompletedTask;
         }
 
         Task IMenu.OnExitAsync()
         {
+            m_BeginMenuUI.OnPlayAudio -= PlaySfx;
+
             return Task.CompletedTask;
+        }
+
+        private void PlaySfx(int id)
+        {
+            m_MenuModule.PlaySfx(id);
         }
     }
 }
