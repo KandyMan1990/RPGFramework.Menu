5250d6c0fa8adb9612ffdfbf35efaef6b6b9786e
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-12-07 18:33:10 +0000

    Update BeginMenuUI with changes from base

--- Runtime/SubMenus/UI/BeginMenuUI.cs
+++ Runtime/SubMenus/UI/BeginMenuUI.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using RPGFramework.Audio;
 using RPGFramework.Core.Input;
 using RPGFramework.Core.UI;
@@ -9,7 +8,7 @@ using UnityEngine.UIElements;
 
 namespace RPGFramework.Menu.SubMenus.UI
 {
-    public class BeginMenuUI : MenuUI<IBeginMenu>, IBeginMenuUI
+    public class BeginMenuUI : MenuUI<IBeginMenuUI>, IBeginMenuUI
     {
         event Action IBeginMenuUI.OnNewGame
         {
@@ -44,53 +43,38 @@ namespace RPGFramework.Menu.SubMenus.UI
         private RPGUIButton m_QuitBtn;
         private Label       m_VersionLabel;
 
-        private IBeginMenuLocalisationArgs m_LocalisationArgs;
-
-        protected override VisualElement ElementToFocusOnEnter => m_NewGameBtn;
+        protected override VisualElement GetDefaultFocusedElement() => m_NewGameBtn;
 
         public BeginMenuUI(IBeginMenuLocalisationArgs localisationArgs,
                            IMenuUIProvider            uiProvider,
                            IGenericAudioIdProvider    audioIdProvider,
-                           ILocalisationService       localisationService) : base(uiProvider,
+                           ILocalisationService       localisationService) : base(localisationArgs,
+                                                                                  uiProvider,
                                                                                   audioIdProvider,
                                                                                   localisationService)
-        {
-            m_LocalisationArgs = localisationArgs;
-        }
+        { }
 
-        protected override Task OnEnterAsync(VisualElement rootContainer)
+        protected override void HookupUI()
         {
-            m_RootUI       = rootContainer.Q<VisualElement>("BeginMenu");
-            m_TitleLabel   = m_RootUI.Q<Label>("TitleLabel");
-            m_NewGameBtn   = m_RootUI.Q<RPGUIButton>("NewGameBtn");
-            m_LoadGameBtn  = m_RootUI.Q<RPGUIButton>("LoadGameBtn");
-            m_SettingsBtn  = m_RootUI.Q<RPGUIButton>("SettingsBtn");
-            m_QuitBtn      = m_RootUI.Q<RPGUIButton>("QuitBtn");
-            m_VersionLabel = m_RootUI.Q<Label>("VersionLabel");
+            m_TitleLabel   = m_UIInstance.Q<Label>("TitleLabel");
+            m_NewGameBtn   = m_UIInstance.Q<RPGUIButton>("NewGameBtn");
+            m_LoadGameBtn  = m_UIInstance.Q<RPGUIButton>("LoadGameBtn");
+            m_SettingsBtn  = m_UIInstance.Q<RPGUIButton>("SettingsBtn");
+            m_QuitBtn      = m_UIInstance.Q<RPGUIButton>("QuitBtn");
+            m_VersionLabel = m_UIInstance.Q<Label>("VersionLabel");
 
             m_VersionLabel.text = $"v{Application.version}";
-
-            foreach (string sheetName in m_LocalisationArgs.DataSheetsToLoad)
-            {
-                m_LocalisationService.LoadNewLocalisationData(sheetName);
-            }
-
-            return Task.CompletedTask;
         }
-
-        protected override Task OnSuspendAsync()
+        
+        protected override void LocaliseUI()
         {
-            return Task.CompletedTask;
-        }
-
-        protected override Task OnResumeAsync()
-        {
-            return Task.CompletedTask;
-        }
-
-        protected override Task OnExitAsync()
-        {
-            return Task.CompletedTask;
+            IBeginMenuLocalisationArgs args = (IBeginMenuLocalisationArgs)m_LocalisationArgs;
+            
+            m_TitleLabel.text  = m_LocalisationService.Get(args.GameTitle);
+            m_NewGameBtn.text  = m_LocalisationService.Get(args.NewGame);
+            m_LoadGameBtn.text = m_LocalisationService.Get(args.LoadGame);
+            m_SettingsBtn.text = m_LocalisationService.Get(args.Settings);
+            m_QuitBtn.text     = m_LocalisationService.Get(args.QuitGame);
         }
 
         protected override void RegisterCallbacks()
@@ -107,15 +91,6 @@ namespace RPGFramework.Menu.SubMenus.UI
             UIToolkitInputUtility.UnregisterButtonCallbacks(m_SettingsBtn, OnSettingsBtnNavigate, OnSettingsBtnSubmitted, OnSettingsBtnClicked, OnSettingsBtnFocus);
             UIToolkitInputUtility.UnregisterButtonCallbacks(m_LoadGameBtn, OnLoadGameBtnNavigate, OnLoadGameBtnSubmitted, OnLoadGameBtnClicked, OnLoadGameBtnFocus);
             UIToolkitInputUtility.UnregisterButtonCallbacks(m_NewGameBtn,  OnNewGameBtnNavigate,  OnNewGameBtnSubmitted,  OnNewGameBtnClicked,  OnNewGameBtnFocus);
-        }
-
-        protected override void LocaliseUI()
-        {
-            m_TitleLabel.text  = m_LocalisationService.Get(m_LocalisationArgs.GameTitle);
-            m_NewGameBtn.text  = m_LocalisationService.Get(m_LocalisationArgs.NewGame);
-            m_LoadGameBtn.text = m_LocalisationService.Get(m_LocalisationArgs.LoadGame);
-            m_SettingsBtn.text = m_LocalisationService.Get(m_LocalisationArgs.Settings);
-            m_QuitBtn.text     = m_LocalisationService.Get(m_LocalisationArgs.QuitGame);
         }
 
         private void OnNewGameBtnNavigate(NavigationMoveEvent evt)
