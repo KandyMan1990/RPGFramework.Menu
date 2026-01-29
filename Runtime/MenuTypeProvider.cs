2a4c44c8b30e42bdb13e98977de784cd2122132e
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-01-29 21:38:46 +0000

    Update MenuTypeProvider.cs

--- Runtime/MenuTypeProvider.cs
+++ Runtime/MenuTypeProvider.cs
@@ -5,6 +5,13 @@ namespace RPGFramework.Menu
 {
     public class MenuTypeProvider : IMenuTypeProvider
     {
+        private readonly IMenuTypeProvider m_MenuTypeProvider;
+
+        public MenuTypeProvider()
+        {
+            m_MenuTypeProvider = this;
+        }
+
         Type IMenuTypeProvider.GetType(MenuType type)
         {
             switch (type)
@@ -22,6 +29,13 @@ namespace RPGFramework.Menu
                 default:
                     throw new ArgumentOutOfRangeException($"{nameof(IMenuTypeProvider)}::{nameof(IMenuTypeProvider.GetType)} [{type}] not implemented");
             }
+        }
+
+        Type IMenuTypeProvider.GetType(byte type)
+        {
+            MenuType menuType = (MenuType)type;
+
+            return m_MenuTypeProvider.GetType(menuType);
         }
     }
 }
